5e1057dc0d64b37e4ec934f1ca338a7ec575d4e1
Jonathan Eduardo Cordova Cuevas <joncu731@student.liu.se>
2021-11-12 15:36:01 +0000

    Update uart.c have added extra funtion (migth have broken everything but i cant complie on my home pc right now). The code still need to be complie then tested and I expect alot of error and bugs. TODO check if the code complies then works.

--- AVR_common/uart.c
+++ AVR_common/uart.c
@@ -1,6 +1,12 @@
 #include <avr/io.h>
 #include "uart.h"
 
+
+/*
+ *none of the below code has been tested as of yet need to be tested nor complied and
+ *it is to be viewed as pusod code more or less untill compilation and testing
+ */
+
 void UART_Init(uint8_t interface)
 {
     if (interface == 0)
@@ -55,8 +61,26 @@ void UART_Transmit(uint8_t interface, unsigned char data )
 
 }
 
-unsigned char UART_Receive(uint8_t interface)
-{
+//add intrupts to the transmip part of the uart transmit also this part has not been tested
+void DATA_Transmit(uint8_t interface, struct dataPaket *paket){
+        
+        /*transmits the header paket*/
+        UART_Transmit(interface, paket.header);
+
+        /*transmists the rest of the data*/
+        uint8_t i = 0;
+        while ( i < 8){
+            unsigned char paket = paket.databytes[i];
+            /* Wait for empty transmit buffer */
+             while ( !( UCSR1A & (1<<UDRE1)) )
+            ;
+            UART_Transmit(interface, paket);
+            i = i + 1;
+        }
+}
+
+unsigned char UART_Receive(uint8_t interface){
+
     if (interface == 0)
     {
         /* Wait for data to be received */
@@ -74,4 +98,31 @@ unsigned char UART_Receive(uint8_t interface)
         return UDR1;
     }
 
+}
+
+
+//TODO add parity checkers and intrupts to this part also this has not been tested
+dataPaket DATA_Receive( unti8_t interface ){
+    dataPaket ReceivedPaket;
+    ReceivedPaket.header =  UART_Receive(uint8_t interface);
+     /*transmists the rest of the data*/
+    uint8_t i = 0;
+    while ( i < 8){
+        /* Wait for data to be received */
+        while ( !(UCSR1A & (1<<RXC1)) )
+            ;
+        unsigned char recivedpaket = ART_Receive(uint8_t interface);
+        paket.datapaket[i] = recivedpaket;
+    }
+
+}
+
+
+//I have no idea if this part will or works at all but something like this could be used check back when trying to complie in atmel
+void parityError( uint8_t interface, struct dataPaket *paket ){
+    byteUnion headerbyte = paket.header;
+    dataPaket erroPaket;
+    erroPaket.header = ( 7<<1 ) | ( 6<<1 ) | ( 5<<1 ) | ( 4<<1 ) | ( 3<<0 ) | ( 2<<0 ) | ( 1<<1 ) | ( 0 ); //correct bits need to be set here 
+    erroPaket.datapaket[0] = ( 3<<headerbyte.b7 ) | (2<<headerbyte.b6) | (1<<headerbyte.b5) | ( headerbyte.b4 );
+    DATA_Transmit( erroPaket, interface );
 }
