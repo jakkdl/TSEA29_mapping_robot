7758a3f86bca0a82830df58dbb3365cb48734976
Klas Johansson <klajo125@student.liu.se>
2021-11-16 15:25:12 +0100

    Added fully functional wheel control system

--- Navigation_unit/main.c
+++ Navigation_unit/main.c
@@ -1,11 +1,24 @@
-
-
 // Main function when running normally, i.e. not running unit tests
 // Will probably be pretty minimal
+#define F_CPU 8000000UL
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+#include "pwm_timer.h"
+uint8_t g_leftSpeed = 0S;
+uint8_t g_rightSpeed = 0;
 int main(void)
 {
     // set up uart interrupts
-
-    return 0;
+	PinInitPWM();
+	sei();
+    while(1)
+	{
+	}
 }
 
+ISR(TIMER0_OVF_vect)
+{
+	OCR0A = g_leftSpeed;
+	OCR0B = g_rightSpeed;
+}
