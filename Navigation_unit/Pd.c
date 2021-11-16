21d0686094a009db221ae20afda8e8646eadadb8
Jonathan Eduardo Cordova Cuevas <joncu731@student.liu.se>
2021-11-16 09:53:44 +0000

    Update Pd.c to now use a different data type and should now be better compatible with rest of nav_unit.
    Pdctroller_update has been remade and should now spit out a number that is how hard we need to turn it requires a target node refernce node and current pos.
    
    TODO test that it actually Works not sure if it while complie i got some wierd erros i didnt have time to fix for this commit.

--- Navigation_unit/Pd.c
+++ Navigation_unit/Pd.c
@@ -1,41 +1,54 @@
 
+#include <stdint.h>
+#include "Pd.h"
 
-#include "PD.h"
-
-void PDController_Init(PIDController *pid) {
+void PDController_Init( PDcontroller *pd ) {
 
 	/* Clear controller internal memory */
-	pd->prevError  = 0.0f;
-	pd->out = 0.0f;
+	pd->Kd = 0; //set this to a good value at a later time
+	pd->Kp = 0; //set this to a good value at a later time
+	pd->PrevCTE = 0;
+	pd->out = 0.;
 
 }
 
-float PDcontroller_Update(Pdcontrller *pd, float targetPos, float currentPos){
 
-    /*
-    * e(t) = r(t) - u(t) Error signal
+
+void PDcontroller_Update(  PDcontroller *pd, int16_t referencePosX, int16_t referencePosY, int16_t targetPosX, int16_t targetPosY, int16_t currentPosX, int16_t currentPosY ){
+	
+	/* calculate the dot product between reference node one and target node with respect to current pos */
+	int16_t deltaX = targetPosX - referencePosX;
+	int16_t deltaY = targetPosY - referencePosY;
+	
+	int16_t RX = currentPosX - targetPosX;
+	int16_t RY = currentPosY - targetPosY;
+	
+	/* cross track error for current iteration */
+	int16_t CTE = ( RY*deltaX - RX*deltaY ) / ( deltaX*deltaX + deltaY*deltaY );
+	
+	/*
+	* int16_t u = ( RX*deltaX - RY*deltaY ) / ( deltaX*deltaX + deltaY*deltaY );
+	*/
+	
+	/*
+    * e(t) = r(t) - u(t) Error signal (this part needed?)
     */
-    float error = targetPos - currentPos;
-
+	
     /*
     * Kp*error proptional part of pd controller
     */
-    float proptional = pd->Kp * error;
+    int16_t proptional = pd->Kp * CTE;
 
     /*
     * Kp*derivative error is the dervitave part of the pd controller
     */
-    float derivative = pd->Kd * (error - prevError);
+    int16_t derivative = pd->Kd * ( CTE - pd->PrevCTE );
 
     /*
-    * U(out) = propitonal part + derivitave part
+    * U(out) = proptional part + derivative part
     */
-    float out = proptional + derivative;
-
-    pd->out = out;
-    pd->prevError = error;
-
-    return out;
+    pd->out = proptional + derivative;
+    pd->PrevCTE = CTE;
 
 } 
 
