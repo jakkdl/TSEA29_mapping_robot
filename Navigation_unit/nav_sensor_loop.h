34ddcb09ab31b3d5803951dcaf0367d7ec78bcd1
h00701350103 <h6+github@protonmail.com>
2021-11-22 14:10:06 +0100

    more files I forgot to add

---
+++ Navigation_unit/nav_sensor_loop.h
@@ -0,0 +1,4 @@
+#ifndef NAVIGATION_UNIT_NAV_SENSOR_LOOP_H_
+#define NAVIGATION_UNIT_NAV_SENSOR_LOOP_H_
+int8_t handle_sensor_data(struct data_packet* data);
+#endif // NAVIGATION_UNIT_NAV_SENSOR_LOOP_H_
---
+++ Navigation_unit/rotation_math.h
@@ -0,0 +1,7 @@
+#ifndef NAVIGATION_UNIT_ROTATION_MATH_H_
+#define NAVIGATION_UNIT_ROTATION_MATH_H_
+
+int8_t calculate_heading_and_position(struct sensor_data *data);
+int8_t update_map(struct sensor_data *data);
+
+#endif // NAVIGATION_UNIT_ROTATION_MATH_H_
