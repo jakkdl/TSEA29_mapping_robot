50da5eafec1c5fcae459baea81aae8309bf0ca1b
Jonathan Cordova <joncu731@student.liu.se>
2021-12-08 22:16:17 +0100

    added logic we should now not crash the thread and paket miss match should be appended to file. Also we now check if the first byte we read is a valid header byte acording to desing doc.

--- Display_unit/debugtest.py
+++ Display_unit/debugtest.py
@@ -1,9 +1,7 @@
-from struct import pack
 import serial
 import threading
 import time
 
-from serial.serialutil import Timeout
 
 #port defention dont changed things beside port
 ser = serial.Serial(
@@ -14,14 +12,6 @@ ser = serial.Serial(
     bytesize=serial.EIGHTBITS,
     timeout=1
 )
-
-"""time out timmer"""
-g_timeout = 0.5
-
-"""CHange here to adjust times"""
-g_time_stop = 1 #time before stop is sent
-g_time_sample = 1 #time before we print out g_output
-g_time_delay = 0.5 #delay after the stop has ben sent to when we read all info in g_output
 
 """glboal static vars"""
 g_output = [] #will contian all pakets in following format [addres, byte count, paket0, ... , paket6] up to 7 pakets
@@ -35,26 +25,29 @@ def listener():
 
         out = []
         temp = ser.read()[0]
+        if valid_header(temp):
+            # bitshift right and mask away everything that is not part of the 4-bit address
+            addr = ((temp >> 4) & 0x0F)
+            # bitshift right and mask away everything that is not part of the 3-bit byte count
+            count = ((temp >> 1) & 0x07)
 
-        # bitshift right and mask away everything that is not part of the 4-bit address
-        addr = ((temp >> 4) & 0x0F)
-        # bitshift right and mask away everything that is not part of the 3-bit byte count
-        count = ((temp >> 1) & 0x07)
-
-        # out.append(temp)
-        out.append(addr)
-        out.append(count)
-        i = 0
-        while(i < count):
-            temp = ser.read()
-            if len(temp):
-                result = temp[0]
-            else:
-                break
-            out.append(result)
-            i += 1
-        g_output.append(out)
-
+            # out.append(temp)
+            out.append(addr)
+            out.append(count)
+            i = 0
+            while(i < count):
+                temp = ser.read()
+                if len(temp):
+                    result = temp[0]
+                else:
+                    break
+                out.append(result)
+                i += 1
+            g_output.append(out)
+            
+        else:
+            print("In valid header recived not printe to file: ", temp)
+ 
 def uint16_to_int16(value):
     if (value > 32768):
         return value - 65536
@@ -78,86 +71,145 @@ def consolOut():
             if g_output[0][0] == 12:
                     #pd paket
                 if g_output[0][2] == 255:
-                    nrOut = "\nPropotional: " + str(uint16_to_int16(g_output[0][4] << 8 | g_output[0][3]))
-                    nrOut = nrOut + " \nDerivative: " + str(uint16_to_int16(g_output[0][6] << 8 | g_output[0][5]))
-                    #nrOut = nrOut + " \nCTE: " + str( float(g_output[0][8] << 8 | g_output[0][9]) )
-                    
+                    if len(g_output[0]) == 7:
+                        nrOut = "\nPropotional: " + str(uint16_to_int16(g_output[0][4] << 8 | g_output[0][3]))
+                        nrOut = nrOut + " \nDerivative: " + str(uint16_to_int16(g_output[0][6] << 8 | g_output[0][5]))
+                        #nrOut = nrOut + " \nCTE: " + str( float(g_output[0][8] << 8 | g_output[0][9]) )
+                    else:
+                        nrOut = "paket miss match: " + str(g_output[0])
+
                     #Navigation goal paket
                 elif g_output[0][2] == 254:
-                    nrOut = "\nNavigationGoal X: " + str(g_output[0][4] << 8 | g_output[0][3])
-                    nrOut = nrOut + " \nNavigationGoal Y: " + str(g_output[0][6] << 8 | g_output[0][5])
+                    if len(g_output[0]) == 7:
+                        nrOut = "\nNavigationGoal X: " + str(g_output[0][4] << 8 | g_output[0][3])
+                        nrOut = nrOut + " \nNavigationGoal Y: " + str(g_output[0][6] << 8 | g_output[0][5])
+                    else:
+                        nrOut = "paket miss match: " + str(g_output[0])
+
                     #reference palet
                 elif g_output[0][2] == 253:
-                    nrOut = "\nReference Pos X: " + str(g_output[0][4] << 8 | g_output[0][3])
-                    nrOut = nrOut + " \nReference Pos Y: " + str(g_output[0][6] << 8 | g_output[0][5])
+                    if len(g_output[0]) == 7:
+                        nrOut = "\nReference Pos X: " + str(g_output[0][4] << 8 | g_output[0][3])
+                        nrOut = nrOut + " \nReference Pos Y: " + str(g_output[0][6] << 8 | g_output[0][5])
+                    else:
+                        nrOut = "paket miss match: " + str(g_output[0])
+
+                    #nav goal heading paket
                 elif g_output[0][2] == 252:
-                    nrOut = nrOut + " \nNavigationGoalHeading: " + str(g_output[0][4] << 8 | g_output[0][3])
+                    if len(g_output[0]) == 5:
+                        nrOut = nrOut + " \nNavigationGoalHeading: " + str(g_output[0][4] << 8 | g_output[0][3])
+                    else:
+                        nrOut = "paket miss match: " + str(g_output[0])
+
+                    #debugs id followed by int 16
                 elif 42 <= g_output[0][2] <= 46:
-                     nrOut = str(g_output[0][2]) + " " + str(uint16_to_int16(g_output[0][4] << 8 | g_output[0][3]))
+                    if len(g_output[0]) == 5:
+                        nrOut = str(g_output[0][2]) + " " + str(uint16_to_int16(g_output[0][4] << 8 | g_output[0][3]))
+                    else:
+                        nrOut = "paket miss match: " + str(g_output[0])
+                    
+                    #break paket end of pd loop
                 elif g_output[0][2] == 100:
                      nrOut = "\n"*10
+
+                    #generic debug id + uint 16
                 elif len(g_output[0]) == 5:
-                    nrOut = str(g_output[0][2]) + " " + str(g_output[0][4] << 8 | g_output[0][3])
+                    nrOut = "Debug: " + str(g_output[0][2]) + " " + str(g_output[0][4] << 8 | g_output[0][3])
+                    
+                    #debug with unknow id and none generic
                 else:
-                    nrOut = str(g_output[0])
+                    nrOut = "unknow debug: " + str(g_output[0])
 
             #lidar forward
             elif g_output[0][0] == 0:
-                nrOut = g_output[0][3] << 8 | g_output[0][2]
-                nrOut = "Lidar Forward: " + str(nrOut)
+                if len(g_output[0]) == 4:
+                    nrOut = g_output[0][3] << 8 | g_output[0][2]
+                    nrOut = "Lidar Forward: " + str(nrOut)
+                else:
+                    nrOut = "Lidar Forward paket miss match " + str(g_output[0])
                 
             # lidar backwards
             elif g_output[0][0] == 1:
-                nrOut = g_output[0][3] << 8 | g_output[0][2]
-                nrOut = "Lidar Backwards: " + str(nrOut)
+                if len(g_output[0]) == 4:
+                    nrOut = g_output[0][3] << 8 | g_output[0][2]
+                    nrOut = "Lidar Backwards: " + str(nrOut)
+                else:
+                    nrOut = "Lidar Backwards paket miss match " + str(g_output[0]) 
 
                 # IR front left
             elif g_output[0][0] == 2:
-                nrOut = g_output[0][3] << 8 | g_output[0][2]
-                nrOut = "IR Front Left: " + str(nrOut)
+                if len(g_output[0]) == 4:
+                    nrOut = g_output[0][3] << 8 | g_output[0][2]
+                    nrOut = "IR Front Left: " + str(nrOut)
+                else:
+                    nrOut = "IR Front Left paket miss match " + str(g_output[0])
 
                 # IR back left
             elif g_output[0][0] == 3:
-                nrOut = g_output[0][3] << 8 | g_output[0][2]
-                nrOut = "IR Back Left: " + str(nrOut)
+                if len(g_output[0]) == 4:
+                    nrOut = g_output[0][3] << 8 | g_output[0][2]
+                    nrOut = "IR Back Left: " + str(nrOut)
+                else:
+                    nrOut = "IR Back Left paket miss match " + str(g_output[0])
 
                 # IR right front
             elif g_output[0][0] == 4:
-                nrOut = g_output[0][3] << 8 | g_output[0][2]
-                nrOut = "IR Front Right: " + str(nrOut)
+                if len(g_output[0]) == 4:
+                    nrOut = g_output[0][3] << 8 | g_output[0][2]
+                    nrOut = "IR Front Right: " + str(nrOut)
+                else:
+                    nrOut = "IR Front Right paket miss match " + str(g_output[0])
 
                 # IR right back
             elif g_output[0][0] == 5:
-                nrOut = g_output[0][3] << 8 | g_output[0][2]
-                nrOut = "IR Back Right: " + str(nrOut)
+                if len(g_output[0]) == 4:
+                    nrOut = g_output[0][3] << 8 | g_output[0][2]
+                    nrOut = "IR Back Right: " + str(nrOut)
+                else:
+                    nrOut = "IR Back Right paket miss match " + str(g_output[0])
 
                 # gyro
             elif g_output[0][0] == 6:
-                nrOut = g_output[0][3] << 8 | g_output[0][2]
-                nrOut = "Gyro: " + str(nrOut)
+                if len(g_output[0]) == 4:
+                    nrOut = g_output[0][3] << 8 | g_output[0][2]
+                    nrOut = "Gyro: " + str(nrOut)
+                else:
+                    nrOut = "Gyro paket miss match " + str(g_output[0])
 
                 # odometer
             elif g_output[0][0] == 7:
-                nrOut = "L: " + str(g_output[0][3]) + " R: " + str(g_output[0][2])
-                nrOut = "Odometer: " + str(nrOut)
+                if len(g_output[0]) == 4:
+                    nrOut = "L: " + str(g_output[0][3]) + " R: " + str(g_output[0][2])
+                    nrOut = "Odometer: " + str(nrOut)
+                else:
+                    nrOut = "Odometer paket miss match " + str(g_output[0])
 
                 # position
             elif g_output[0][0] == 8:
-                nrOut = "\nPositionX: " + str(g_output[0][3] << 8 | g_output[0][2])
-                nrOut += "\nPositionY: " + str(g_output[0][5] << 8 | g_output[0][4])
-                
+                if len(g_output[0]) == 6:
+                    nrOut = "\nPositionX: " + str(g_output[0][3] << 8 | g_output[0][2])
+                    nrOut += "\nPositionY: " + str(g_output[0][5] << 8 | g_output[0][4])
+                else:
+                    nrOut = "Position paket miss match " + str(g_output[0])
 
                 # direction
             elif g_output[0][0] == 9:
-                nrOut = g_output[0][3] << 8 | g_output[0][2]
-                nrOut = "Direction: " + str(nrOut)
+                if len(g_output[0]) == 4:
+                    nrOut = g_output[0][3] << 8 | g_output[0][2]
+                    nrOut = "Direction: " + str(nrOut)
+                else:
+                    nrOut = "Direction paket miss match " + str(g_output[0])
 
                 # map update
             elif g_output[0][0] == 10:
+                if len(g_output[0]) == 5:
                     nrOut = "Map update: " + str(g_output[0][2]) + " " + str(g_output[0][3]) + " " + str( g_output[0][4] )
+                else:
+                    nrOut = "Map update paket miss match " + str(g_output[0])
 
+                #last case should never happen
             else:
-                nrOut = "Unknow paket: " + str(g_output[0])
+                nrOut = "Unknow paket we should never be here check code: " + str(g_output[0])
             
             #add the current nrout which is our output to debug file
             f = open("debug.txt", "a")
@@ -165,11 +217,19 @@ def consolOut():
             f.close()
             g_output.pop(0)
 
+def valid_header(header):
+    """
+    this function returns true if we recive a valid header
+    last bit is 0
+    4 last bits are a valid address
+    3 first bit are a value of 0 to 7
+    """
+    return ( not( ( header ) & 0x01) ) and ( 0x0 <= ( ( header >> 4 ) & 0x0F ) <= 0xF ) and ( 0 <= ( ( header >> 1 ) & 0x07 ) <= 7)
+
 def packageMaker(operation, byteList):
     """
     this function sends the byte to the fire fly
     """
-    print("in sender")
     global g_dict
 
     listToSend = [g_dict.get(operation)] + byteList
@@ -181,7 +241,7 @@ def packageMaker(operation, byteList):
         ser.write(package)
         print("sent: ", package)
         time.sleep(0.1)
-    time.sleep(1)
+    time.sleep(0.1)
 
 def main():
     """
@@ -193,8 +253,6 @@ def main():
     f.close()
 
     global g_output
-    global g_time
-    global g_timeout
     #start a lisner thread that read port pakets
 
     t1 = threading.Thread(target=listener)
@@ -204,54 +262,34 @@ def main():
     t2.start()
 
     while True:
-        
+        #you can do this part in one print but this also works
         print("-------------------------------------------------------")
+        print("0: Stop")
+        print("1: Start")
+        print("2: Forward")
+        print("3: Backwards")
+        print("4: forward left")
+        print("5: forward right")
+        print("6: rotate left")
+        print("7: rotate right")
+        print("13: Kp")
+        print("14: Kd")
+        print("-------------------------------------------------------")
+
         val = int(input("Enter a value: "))
-        if val == 0:
+        if  0 <= val <= 7:
             threading.Thread(target=packageMaker,
-                                args=("command", [0])).start()
-        elif val == 1:
-            threading.Thread(target=packageMaker,
-                                args=("command", [1])).start()
-
-        elif val == 2:
-            threading.Thread(target=packageMaker,
-                                args=("command", [2])).start()
-
-        elif val == 3:
-            threading.Thread(target=packageMaker,
-                                args=("command", [3])).start()
-
-        elif val == 4:
-            threading.Thread(target=packageMaker,
-                                args=("command", [4])).start()
-
-        elif val == 5:
-  args          threading.Thread(target=packageMaker,
-                                args=("command", [5])).start()
-                                
-        elif val == 6:
-            threading.Thread(target=packageMaker,
-                                args=("command", [6])).start()
-        elif val == 7:
-            threading.Thread(target=packageMaker,
-                                args=("command", [7])).start()
-
-    
+                                args=("command", [ val ])).start()
         elif val == 13:
-            kp = int(input("Enter kp"))
+            kp = int(input("Enter kp: "))
             threading.Thread(target=packageMaker,
-                                args=("kp", [kp])).start()
-                                
+                                args=("kp", [kp])).start()                   
         elif val == 14:
-            kd = int(input("Enter Kd"))
+            kd = int(input("Enter Kd: "))
             threading.Thread(target=packageMaker,
                                 args=("kd", [kd])).start()
         
-        time.sleep(g_time_delay)
+        time.sleep(0.5)
         
-
-        
-
 if __name__ == '__main__':
     main()
