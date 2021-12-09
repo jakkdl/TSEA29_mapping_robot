380cbf08638fe5fa4f53db9ccc9a793f80364e22
Jonathan Cordova <joncu731@student.liu.se>
2021-12-09 19:04:57 +0100

    added graphic option to debug tool and some more logic and did some cleaning

--- Display_unit/debugtest.py
+++ Display_unit/debugtest.py
@@ -1,9 +1,10 @@
 import serial
 import threading
 import time
+from tkinter import *
 
 
-#port defention dont changed things beside port
+port defention dont changed things beside port
 ser = serial.Serial(
     port='/dev/rfcomm0', #this part is where you pu thr firefly port
     baudrate=115200,
@@ -16,6 +17,10 @@ ser = serial.Serial(
 """glboal static vars"""
 g_output = [] #will contian all pakets in following format [addres, byte count, paket0, ... , paket6] up to 7 pakets
 g_dict = {"command": 0xB2, "kd": 0xE2, "kp": 0xD2} #this has the header should not be change unless you know what you are doing
+g_x = -1
+g_y = -1
+g_map_update = False
+g_color = "red"
 
 def listener():
     global g_output
@@ -51,6 +56,11 @@ def listener():
 def uint16_to_int16(value):
     if (value > 32768):
         return value - 65536
+    return value
+
+def uint8_to_int8(value):
+    if (value > 128):
+        return value - 256
     return value
 
 def consolOut():
@@ -171,7 +181,7 @@ def consolOut():
                 # gyro
             elif g_output[0][0] == 6:
                 if len(g_output[0]) == 4:
-                    nrOut = g_output[0][3] << 8 | g_output[0][2]
+                    nrOut = uint16_to_int16( g_output[0][3] << 8 | g_output[0][2] )
                     nrOut = "Gyro: " + str(nrOut)
                 else:
                     nrOut = "Gyro paket miss match " + str(g_output[0])
@@ -203,7 +213,14 @@ def consolOut():
                 # map update
             elif g_output[0][0] == 10:
                 if len(g_output[0]) == 5:
-                    nrOut = "Map update: " + str(g_output[0][2]) + " " + str(g_output[0][3]) + " " + str( g_output[0][4] )
+                    nrOut = "Map update: " + str(g_output[0][2]) + " " + str(g_output[0][3]) + " " + str( uint8_to_int8( g_output[0][4] ) )
+                    g_x = g_output[0][2]
+                    g_y = g_output[0][3]
+                    if uint8_to_int8( g_output[0][4] ) < 0:
+                        g_color = "blue"
+                    else:
+                        g_color = "green"
+                    g_map_update = True
                 else:
                     nrOut = "Map update paket miss match " + str(g_output[0])
 
@@ -216,6 +233,7 @@ def consolOut():
             f.write(nrOut + "\n")
             f.close()
             g_output.pop(0)
+
 
 def valid_header(header):
     """
@@ -230,8 +248,6 @@ def packageMaker(operation, byteList):
     """
     this function sends the byte to the fire fly
     """
-    global g_dict
-
     listToSend = [g_dict.get(operation)] + byteList
     print("list: ", listToSend)
 
@@ -252,15 +268,77 @@ def main():
     f.write("")
     f.close()
 
-    global g_output
     #start a lisner thread that read port pakets
-
     t1 = threading.Thread(target=listener)
     t1.start()
 
     t2 = threading.Thread(target=consolOut)
-    t2.start()
+    
+    print("Use graphic")
+    print("0: No")
+    print("1: Yes")
+    val = int(input("Enter value: "))
+    if  val == 1:
+        t4 = threading.Thread(target=graphic)
+        
+    t3 = threading.Thread(target=menu)
+    t3.start()
 
+    t4.start()
+
+    t2.start()
+    
+
+     
+def graphic():
+    # graphics
+    root = Tk()
+    navMap = Map(root).grid(row=0, column=0, padx=5, pady=5)
+    root.title("Gudrid Interface")
+    root.mainloop()
+
+class Constants:
+    DELAY = 300
+    CELL_SIZE = 20
+
+class Map(LabelFrame):
+
+    """Graphical representation of the robot's movement and the room"""
+
+    def __init__(self, parent):
+        """Constructor"""
+        LabelFrame.__init__(self, parent)
+        self.parent = parent
+        self.after(Constants.DELAY, self.onTimer)
+        CELL_SIZE = 20
+        SPACING = 4
+        self.canvas = Canvas(self, width=CELL_SIZE * 49, height=CELL_SIZE * 25)
+
+        for x in range(49):
+            for y in range(25):
+
+                # Create an "xy" tag for each rectangle on the map
+                coord = (x, y)
+                stringTuple = tuple(map(str, coord))
+                tag = stringTuple[0] + "," + stringTuple[1]
+
+                self.canvas.create_rectangle(x * CELL_SIZE + SPACING, y * CELL_SIZE + SPACING,
+                                             (x + 1)*CELL_SIZE, (y + 1)*CELL_SIZE, fill='gray', width=0, tags=tag)
+        self.canvas.pack(side=LEFT)
+
+    def onTimer(self):
+        global g_map_update
+        if g_map_update:
+            square = self.canvas.find_withtag(
+                        str( g_x ) + "," + str( g_y ))
+            self.canvas.itemconfig(square, fill=g_color)
+            self.after(Constants.DELAY, self.onTimer)
+            g_map_update = False
+
+
+
+def menu():
+    global g_dict
     while True:
         #you can do this part in one print but this also works
         print("-------------------------------------------------------")
@@ -290,6 +368,7 @@ def main():
                                 args=("kd", [kd])).start()
         
         time.sleep(0.5)
+
         
 if __name__ == '__main__':
     main()
